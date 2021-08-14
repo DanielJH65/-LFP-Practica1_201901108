a27d9ac705694c4f32ae17ee6810373a399ad64d
Daniel Jiménez <wdaniel.jimenez@gmail.com>
2021-08-13 18:55:51 -0600

    Leer Archivos

--- main.py
+++ main.py
@@ -1,4 +1,37 @@
-from menu import Menu
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+import webbrowser
 
 if __name__ == "__main__":
-    Menu()
+    opcion = 0
+    while(opcion != 4):
+        opcion = int(input("""
+###############--Menú--###############
+#                                    #
+#   Elija una opción:                #
+#   1) Cargar archivo de entrada     #
+#   2) Mostrar Reporte en Consola    #
+#   3) Exportar Reporte              #
+#   4) Salir                         #
+#                                    # 
+######################################  
+    """))
+
+        if opcion == 1:
+            Tk().withdraw()
+            archivo = askopenfilename()
+            archivo = open(archivo, "r")
+            datos = archivo.read()
+            archivo.close()
+            separar_datos(datos, "\n")
+        elif opcion == 2:
+            pass
+        elif opcion == 3:
+            pass
+        elif opcion == 4:
+            break
+        else:
+            print("\nDebe ingresar un número valido")
+
+def separar_datos(datos, separador):
+    temp = ""
