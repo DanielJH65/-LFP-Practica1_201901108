c77ce4177002b68b27fd2a3470eab16857279648
Daniel Jiménez <wdaniel.jimenez@gmail.com>
2021-08-13 22:10:13 -0600

    Entrada Completa

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 import webbrowser
 
+class Alumno:
+    def __init__(self, nombre, nota):
+        self.nombre = nombre
+        self.nota = nota
+
 def abrirArchivo():
     Tk().withdraw()
     archivo = askopenfilename()
@@ -24,9 +29,32 @@ def separarDatos(datos, separador):
         listaTemp.append(temp.strip())
     return listaTemp
 
+def separarAlumnos(datos):
+    alumnos1 = []
+    for i in datos:
+        i = i.replace("<","")
+        i = i.replace(">","")
+        i = i.replace(",","")
+        i = i.replace('"',"")
+        alumnos1.append(separarDatos(i,";"))
+    return alumnos1
+
+def crearObjetos(alumnos1):
+    listafinal = []
+    for temp in alumnos1:
+        alumno = Alumno(temp[0], int(temp[1]))
+        listafinal.append(alumno)
+    return listafinal
+
+def salidaConsola(curso, alumnos, parametros):
+    pass
+
 if __name__ == '__main__':
     opcion = 0
     curso = ""
+    curso = ""
+    parametros = []
+    alumnosFinal = []
     while(opcion != 4):
         opcion = int(input("""
 ###############--Menú--###############
@@ -45,10 +73,14 @@ if __name__ == '__main__':
             curso = separados1[0].replace("{","")
             curso = curso.replace("=","")
             separados1.pop(0)
-            print(curso)
-            print(separados1)
+            longitud = len(separados1)
+            parametro = separados1[longitud-1].replace("}","")
+            parametros = separarDatos(parametro,",")
+            separados1.pop(longitud-1)
+            alumnos1 = separarAlumnos(separados1)
+            alumnosFinal = crearObjetos(alumnos1)
         elif opcion == 2:
-            pass
+            salidaConsola(curso, alumnosFinal, parametros)
         elif opcion == 3:
             pass
         elif opcion == 4:
