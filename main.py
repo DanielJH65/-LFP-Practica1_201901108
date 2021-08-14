e792630c048c87639c063f3d0452dfb27eeb66fe
Daniel Jiménez <wdaniel.jimenez@gmail.com>
2021-08-13 19:20:37 -0600

    Separar Curso

--- main.py
+++ main.py
@@ -2,8 +2,31 @@ from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 import webbrowser
 
-if __name__ == "__main__":
+def abrirArchivo():
+    Tk().withdraw()
+    archivo = askopenfilename()
+    archivo = open(archivo, "r")
+    datos = archivo.read()
+    archivo.close()
+    return datos
+
+def separarDatos(datos, separador):
+    temp = ""
+    listaTemp = []
+
+    for char in  datos:
+        if char == separador:
+            listaTemp.append(temp.strip())
+            temp = ""
+        else:
+            temp += char
+    if temp.strip() != "":
+        listaTemp.append(temp.strip())
+    return listaTemp
+
+if __name__ == '__main__':
     opcion = 0
+    curso = ""
     while(opcion != 4):
         opcion = int(input("""
 ###############--Menú--###############
@@ -16,14 +39,14 @@ if __name__ == "__main__":
 #                                    # 
 ######################################  
     """))
-
         if opcion == 1:
-            Tk().withdraw()
-            archivo = askopenfilename()
-            archivo = open(archivo, "r")
-            datos = archivo.read()
-            archivo.close()
-            separar_datos(datos, "\n")
+            datos = abrirArchivo()
+            separados1 = separarDatos(datos, "\n")
+            curso = separados1[0].replace("{","")
+            curso = curso.replace("=","")
+            separados1.pop(0)
+            print(curso)
+            print(separados1)
         elif opcion == 2:
             pass
         elif opcion == 3:
@@ -32,6 +55,3 @@ if __name__ == "__main__":
             break
         else:
             print("\nDebe ingresar un número valido")
-
-def separar_datos(datos, separador):
-    temp = ""
